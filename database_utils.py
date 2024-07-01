448eae875d4912b78df3fffb733fef4139725fc7
Salih <salih@Salihs-Mac.local>
2024-07-01 21:47:44 +0100

    added read_rds_table method that takes and instance of the class DatabaseConnector and table name, then return back a pandas dataframe of the table content

--- database_utils.py
+++ database_utils.py
@@ -58,8 +58,8 @@ class DatabaseConnector:
             dataMap = yaml.safe_load(f)
         
         ## testing-start -> remove
-        for key, value in dataMap.items():
-            print(key, '\t', value)
+        # for key, value in dataMap.items():
+        #     print(key, '\t', value)
         # print(dataMap.get('RDS_HOST'))
         # print(dataMap['RDS_HOST'])
         # testing-end
@@ -87,18 +87,44 @@ class DatabaseConnector:
         from sqlalchemy import inspect
 
         engine = self.init_db_engine()
-        engine.connect()
+        conn = engine.connect()
 
         inspector = inspect(engine)
         db_names_list = inspector.get_table_names()
 
         print(db_names_list)
         
-        return
-    
-def run():
-    myconnection = DatabaseConnector()
-    myconnection.list_db_tables()
+        conn.close()
 
+        return db_names_list
+    
+    def read_db_table(self, db_table):
+        import pandas as pd
+
+        engine = self.init_db_engine()
+        #conn = engine.connect()
+        table_load = pd.read_sql_table(db_table, engine)
+        #conn.close()
+        #print(table_load.head(5))
+
+        return table_load
+    
+def read_rds_table(conn, db_table):
+
+    table_load = conn.read_db_table(db_table)
+
+    return table_load
+    
 if __name__ == '__main__':
-    run()
+    myconnection = DatabaseConnector()
+    
+    db_names_list = myconnection.list_db_tables()
+
+    for db_table in db_names_list:
+        # ['legacy_store_details', 'dim_card_details', 'legacy_users', 'orders_table']
+        print(f'\nReading DB Table :: {db_table} \n' )
+        table_load = read_rds_table(myconnection, db_table)
+        print(table_load.head(5))
+
+
+    
