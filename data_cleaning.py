c9bf92ef15d9be59cfe295e749c5d027ecc31fa3
Salih <salih@Salihs-Mac.local>
2024-07-03 21:17:13 +0100

    added called_clean_store_data for cleaning store data dataframe constructed from api endpoint

--- data_cleaning.py
+++ data_cleaning.py
@@ -91,7 +91,7 @@ class DataCleaning:
         df['email_address'] = df['email_address'].apply(lambda x: x if type(x) != float and '@' in x else np.nan)
 
         ## remove raws for both already existing and newly introduced NaN cells in the conversion above.
-        dfc = df[~df.isnull().any(axis=1)].copy(deep=True)
+        dfc = df[~df.isnull().any(axis=1)].copy(deep=True) #df.dropna(how='any',axis=0, inplace=True) or use .copy() 
 
         return dfc
         #pass
@@ -108,8 +108,27 @@ class DataCleaning:
         return dfc
         #pass
 
-if __name__ == '__main__':
+    def called_clean_store_data(self, df):
+        df['opening_date'] = df['opening_date'].apply(lambda x: self.clean_date(x))
+        df['staff_numbers'] = df['staff_numbers'].apply(lambda x: re.sub('\D', '', x) if ~x.isnumeric() else x) #clean 3n9 for staff_numbers, which should be numeric
+        
+        # remove lat, which is empty and duplicate
+        df.drop('lat', axis=1, inplace=True)
+
+        #rearrange latitude next to longitude
+        col = df.pop('latitude')
+        df.insert(3, col.name, col)
+
+        ## remove raws for both already existing and newly introduced NaN cells in the conversion above.
+        dfc = df.dropna(axis=0, subset=['opening_date']).copy(deep=True) # or use inplace=True
+        #dfc = df[~df.isnull().any(axis=1)].copy(deep=True) #removes all data because lat column is null i.e. empty for all rows! 
+
+        dfc.to_csv('./data/stores_clean.csv', sep=',', index=False, header=True, encoding='utf-8')
+
+        return dfc
     
+if __name__ == '__main__':
+    import pandas as pd
     from database_utils import DatabaseConnector
     from data_extraction import DataExtractor
 
@@ -121,12 +140,13 @@ if __name__ == '__main__':
 
     for db_table in db_names_list:
         # ['legacy_store_details', 'dim_card_details', 'legacy_users', 'orders_table']
-        if db_table == 'legacy_users':
+        if db_table == 'legacy_users' and False:
             print(f'\nReading DB Table :: {db_table} \n' )
             table_load = connector.read_db_table(db_table)
             # print(table_load.head(5))
             dfc_users = cleaner.clean_user_data(table_load)
             #print(dfc.head(5))
+            dfc_users.to_csv('./data/legacy_users_clean.csv', sep=',', index=False, header=True, encoding='utf-8')
             connector.upload_to_db(dfc_users,'dim_users')
     
     pdf_path = 'https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf'
@@ -135,4 +155,21 @@ if __name__ == '__main__':
     
     dfc_pdf = cleaner.clean_card_data(pdf_load)
     connector.upload_to_db(dfc_pdf,'dim_card_details')
+
+    num_stores = extractor.list_number_of_stores()
+    print('Number of Stores ::', num_stores)
+
+    stores_list = []
+    for x in range(0,num_stores):
+        store_data = extractor.retrieve_stores_data(x)
+        stores_list.append(store_data)
+
+    df_stores = pd.DataFrame(stores_list)
+    print(df_stores.head(5))
+
+    dfc_stores = cleaner.called_clean_store_data(df_stores)
+
+    print('\nClean Stores ::')
+    print(dfc_stores.head(5))
+    #dfc_stores.to_csv('./data/stores_clean.csv', sep=',', index=False, header=True, encoding='utf-8')
     #pass
--- data_extraction.py
+++ data_extraction.py
@@ -78,11 +78,39 @@ class DataExtractor:
         print(json.dumps(response.json()))
         return response.json()['number_stores']
 
+    def retrieve_stores_data(self, store_id):
+        import requests
+        import json
+
+        url = "https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/" + str(store_id)
+        headers = {
+            "Content-Type": "application/json",
+            "x-api-key": "yFBQbwXe9J3sd6zWVAMrK6lcxxr0q1lr2PT6DDMX"
+        }
+
+        response = requests.get(url, headers=headers)
+
+        #print(json.dumps(response.json()))
+        return response.json()
+    
 if __name__ == '__main__':
+
+    import pandas as pd
+
     extractor = DataExtractor()
 
     pdf_path = 'https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf'
-    #df = extractor.retrieve_pdf_data(pdf_path)
+    #df_pdf = extractor.retrieve_pdf_data(pdf_path)
 
     num_stores = extractor.list_number_of_stores()
     print('Number of Stores ::', num_stores)
+
+    stores_list = []
+    for x in range(0,num_stores):
+        store_data = extractor.retrieve_stores_data(x)
+        stores_list.append(store_data)
+
+    df_stores = pd.DataFrame(stores_list)
+    print(df_stores.head(5))
+
+    #df_stores.to_csv('./data/stores.csv', sep=',', index=False, header=True, encoding='utf-8')
