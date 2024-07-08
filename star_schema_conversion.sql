430f42e6f544268a4c0689c3758402ab9bfe50f5
Salih <salih@Salihs-Mac.local>
2024-07-08 19:32:24 +0100

    add sql code for modifying dim tables to have primary keys

--- star_schema_conversion.sql
+++ star_schema_conversion.sql
@@ -183,3 +183,20 @@ ALTER TABLE dim_card_details
 	ALTER COLUMN expiry_date TYPE VARCHAR(5),
 	ALTER COLUMN date_payment_confirmed TYPE DATE USING CAST(date_payment_confirmed AS DATE);
 
+-- Modify dim tables with primary keys for columns with equivalents in the orders_table
+ALTER TABLE dim_card_details
+	ADD CONSTRAINT pk_card_nuber PRIMARY KEY (card_number);
+	
+ALTER TABLE dim_date_times
+	ADD PRIMARY KEY (date_uuid);
+	
+ALTER TABLE dim_products
+	ADD PRIMARY KEY (product_code);
+	
+ALTER TABLE dim_store_details
+	ADD PRIMARY KEY (store_code);
+	
+ALTER TABLE dim_users
+	ADD PRIMARY KEY (user_uuid);
+
+    
